e07eff8e0501dd9276d6f4e7873252bdfdb84ee8
Nicholas Battaglia <nbatta@astro.cornell.edu>
2022-05-20 14:36:04 -0400

    added lightcone test

--- testszfm.py
+++ testszfm.py
@@ -6,3 +6,9 @@ cosmo = szfm.cosmology.cosmology()
 hmf = szfm.hmf.hmf(cosmo)
 
 lc = szfm.lightcone.lightcone(cosmo=cosmo,fsky=0.1,Mmin=2e14)
+
+lc.populate(hmf.dndmofmz)
+
+# write halos in pksc halo format used in Websky
+catfile = 'test_cat.pksc'
+lc.write_pksc(catfile=catfile)
